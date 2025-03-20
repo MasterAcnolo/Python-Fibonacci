26b80c2671583dcfa324f866fab1d87b3b3cc195
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-03-20 14:04:40 +0100

    Create fibonaccibis.py

---
+++ fibonaccibis.py
@@ -0,0 +1,20 @@
+a = 0 
+b = 1 
+
+
+print("Voici la Suite de Fibonacci ! ")
+
+num = input("Entrer une valeure de répétition")
+
+if num(type == int):
+    
+    for i in range (num): # Nombre de répétitions de la fonction = num 
+    
+     c = a + b # 3ème Nombre = Somme des deux précédents 
+    
+    
+    print(c ,end=' ') # Print le nombre de la suite 
+    a,b = b, c # Pour continuer la fonction on remplace les valeurs 
+
+else: 
+    print("Veuillez entrer une valeure de type int")
